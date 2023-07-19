1df59783a818aefe7f0fa8947ae17c8a4a69a3d4
Jhonatan_Serpa_Laptop <jhonatanmeireles12@yahoo.com.br>
2023-07-19 01:12:11 -0300

    Inicializando no estudo sobre enumeradores.

---
+++ ConsoleOOP.Aula18_and_19/Enumeradores/OpcaoSistemaEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleOOP.Aula18_and_19.Enumeradores
+{
+    public enum OpcaoSistemaEnum
+    {
+        Salvar = 1,
+        Listar = 2,
+        Excluir = 3,
+        Sair = 4
+    }
+}
---
+++ ConsoleOOP.Aula18_and_19/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleOOP.Aula18_and_19.Enumeradores;
+
+do
+{
+    Console.Clear();
+
+    Console.WriteLine("Escolha a opção abaixo:");
+    Console.WriteLine("(1) Salvar ");
+    Console.WriteLine("(2) Listar ");
+    Console.WriteLine("(3) Excluir ");
+    Console.WriteLine("(4) Sair ");
+    Console.WriteLine(" ");
+    Console.WriteLine("Digite sua opção:");
+    var opcao = (OpcaoSistemaEnum) int.Parse(Console.ReadLine()!);
+
+    if (opcao == OpcaoSistemaEnum.Salvar)
+    {
+        Console.WriteLine("Opção Salvar");
+    }
+    else if (opcao == OpcaoSistemaEnum.Listar)
+    {
+        Console.WriteLine("Opção Listar");
+    }
+    else if (opcao == OpcaoSistemaEnum.Sair)
+    {
+        Console.WriteLine("Opção Sair");
+        Console.ReadKey();
+        Environment.Exit(0);
+    }
+    else if (opcao == OpcaoSistemaEnum.Excluir)
+    {
+        Console.WriteLine("Opção Excluir");
+    }
+
+    Console.ReadKey();
+
+} while (true);
+Console.ReadKey();
